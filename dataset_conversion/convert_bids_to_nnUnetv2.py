5918e2dbb95fd01ae0d3ff19d2f5b7a222af119b
Theo <58122097+tzebre@users.noreply.github.com>
2023-06-19 15:53:12 -0400

    Modified first docstring

--- dataset_conversion/convert_bids_to_nnUnetv2.py
+++ dataset_conversion/convert_bids_to_nnUnetv2.py
@@ -2,15 +2,9 @@
 Converts BIDS-structured dataset to the nnUNetv2 dataset format. Full details about
 the format can be found here: https://github.com/MIC-DKFZ/nnUNet/blob/master/documentation/dataset_format.md
 
-Note that the conversion from BIDS to nnUNet is done using symbolic links to avoid creating multiple copies of the 
-(original) BIDS dataset.
-
-Currently only supports the conversion of a single contrast. In case of multiple contrasts, the script should be 
-modified to include those as well. 
-
 Usage example:
     python convert_bids_to_nnUNetv2.py --path-data ~/data/dataset --path-out ~/data/dataset-nnunet
-                    --dataset-name MyDataset --dataset-number 501 --split 0.6 0.2 --seed 99
+                    --dataset-name MyDataset --dataset-number 501 --split 0.6 0.2 --seed 99 --copy False
 
 Naga Karthik, Jan Valosek modified by Théo Mathieu
 """
