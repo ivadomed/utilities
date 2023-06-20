e6fd3f7f8eacc490b5ef2531023e7cac39816971
Theo <58122097+tzebre@users.noreply.github.com>
2023-06-20 16:24:38 -0400

    start docstring and start separating label file in different files depending on voxel value

--- dataset_conversion/nnunetV2_to_bids.py
+++ dataset_conversion/nnunetV2_to_bids.py
@@ -5,14 +5,15 @@ the format can be found here: https://github.com/MIC-DKFZ/nnUNet/blob/master/doc
 Théo Mathieu
 """
 
-import re
 import argparse
+import numpy as np
 import shutil
 import pathlib
 from pathlib import Path
 import datetime
 import json
 import os
+import nibabel as nib
 
 
 def get_parser():
@@ -26,16 +27,42 @@ def get_parser():
     return parser
 
 
-def separate_labels(label_file, dataset_label):
+def separate_labels(label_file, original_label, dataset_label, label_new_dir, dataset_name):
+    """
+    Function to make one nifti file for each possible voxel value
+
+    Args:
+        label_file (str): Path to the label file ...label-
+        original_label (str): Path to the label file in the nnUNetV2 dataset
+        dataset_label (str): Labels keys from the dataset.json file
+        label_new_dir (str): Folder for the label file in Bids format
+        dataset_name (str): nnUNetV2 dataset name
+    """
+    # TODO for region based segmentation add the different part to make one part
+    # (this issue: https://github.com/ivadomed/data-conversion/pull/15#issuecomment-1599351103)
     value_label = {v: k for k, v in dataset_label.items()}
+    nifti_file = nib.load(original_label)
+    data = nifti_file.get_fdata()
     for value in value_label.keys():
         if value != 0:
-            print(value_label[value])
-            #TODO create new nifti image with only this value and _label-manual.nii.gz
-    return 0
+            seg_name = value_label[value]
+            voxel_val = np.zeros_like(data)
+            voxel_val[data == value] = 1
+            voxel_img = nib.Nifti1Image(voxel_val, nifti_file.affine, nifti_file.header)
+            path_to_label = os.path.join(label_new_dir, f"{label_file}-{seg_name}_seg.nii.gz")
+            nib.save(voxel_img, path_to_label)
+            json_name = f"{label_file}-{seg_name}_seg.json"
+            write_json(os.path.join(label_new_dir, json_name), dataset_name)
 
 
 def write_json(filename, dataset_name):
+    """
+    Save a json file with the label image created
+
+    Args:
+        filename (str): Json filename (with path)
+        dataset_name (str): Name of the dataset
+    """
     data = {
         "Author": f"nnUNetV2_to_bids.py (git link?) from nnUNet dataset {dataset_name}",
         "Date": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -47,6 +74,21 @@ def write_json(filename, dataset_name):
 
 
 def get_subject_info(file_name, contrast_dict):
+    """
+    Get different information about the current subject
+
+    Args:
+        file_name (str): Filename corresponding to the subject image
+        contrast_dict (dict): Dictionary, key channel_names from dataset.json
+
+    Returns:
+        sub_names (str): Name of the subject. Example: sub-milan002
+        ses (str): ses name
+        bids_nb (str): subject number in the BIDS dataset
+        info[2] (str): Contrast value in BIDS format. Example 0001
+        contrast (str): Image contrast (T2w, T1, ...)
+
+    """
     name = file_name.split(".")[0]
     info = name.split("_")
     if len(info) == 4:
@@ -70,7 +112,6 @@ def main():
     path_out = Path(os.path.abspath(os.path.expanduser(args.path_out)))
     with open(os.path.join(root, "dataset.json"), 'r') as json_file:
         dataset_info = json.load(json_file)
-    separate_labels("_", dataset_info["labels"])
     for folder in [("imagesTr", "labelsTr"), ("imagesTs", "labelsTs")]:
         for image_file in os.listdir(f"{root}/{folder[0]}/"):
             sub_name, ses, bids_nb, bids_contrast, contrast = get_subject_info(image_file,
@@ -80,28 +121,30 @@ def main():
             if ses:
                 image_new_dir = os.path.join(path_out, sub_name, ses, 'anat')
                 label_new_dir = os.path.join(path_out, 'derivatives/labels', sub_name, ses, 'anat')
+                pathlib.Path(image_new_dir).mkdir(parents=True, exist_ok=True)
+                pathlib.Path(label_new_dir).mkdir(parents=True, exist_ok=True)
                 bids_image_name = f"{sub_name}_{ses}_{contrast}.nii.gz"
-                bids_label_name = f"{sub_name}_{ses}_{contrast}_label-manual.nii.gz"
-                json_name = f"{sub_name}_{ses}_{contrast}_label-manual.json"
                 label_name = f"{sub_name}_{ses}_{bids_nb}.nii.gz"
+                label_file = os.path.join(root, folder[1], label_name)
+                separate_labels(f"{sub_name}_{ses}_{contrast}_label", label_file, dataset_info["labels"], label_new_dir,
+                                str(root).split('/')[-1])
             else:
                 image_new_dir = os.path.join(path_out, sub_name, 'anat')
                 label_new_dir = os.path.join(path_out, 'derivatives/labels', sub_name, 'anat')
+                pathlib.Path(image_new_dir).mkdir(parents=True, exist_ok=True)
+                pathlib.Path(label_new_dir).mkdir(parents=True, exist_ok=True)
                 bids_image_name = f"{sub_name}_{contrast}.nii.gz"
-                bids_label_name = f"{sub_name}_{contrast}_label-manual.nii.gz"
-                json_name = f"{sub_name}_{contrast}_label-manual.json"
                 label_name = f"{sub_name}_{bids_nb}.nii.gz"
+                label_file = os.path.join(root, folder[1], label_name)
+                separate_labels(f"{sub_name}_{contrast}_label", label_file, dataset_info["labels"], label_new_dir,
+                                str(root).split('/')[-1])
             image_file = os.path.join(root, folder[0], image_file)
-            label_file = os.path.join(root, folder[1], label_name)
-            pathlib.Path(image_new_dir).mkdir(parents=True, exist_ok=True)
-            pathlib.Path(label_new_dir).mkdir(parents=True, exist_ok=True)
-            write_json(os.path.join(label_new_dir, json_name), str(root).split('/')[-1])
             if copy:
                 shutil.copy2(os.path.abspath(image_file), os.path.join(image_new_dir, bids_image_name))
-                shutil.copy2(os.path.abspath(label_file), os.path.join(label_new_dir, bids_label_name))
+                # shutil.copy2(os.path.abspath(label_file), os.path.join(label_new_dir, bids_label_name))
             else:
                 os.symlink(os.path.abspath(image_file), os.path.join(image_new_dir, bids_image_name))
-                os.symlink(os.path.abspath(label_file), os.path.join(label_new_dir, bids_label_name))
+                # os.symlink(os.path.abspath(label_file), os.path.join(label_new_dir, bids_label_name))
 
 
 if __name__ == '__main__':
