c151d8d2462504fe0927f6eda1d7f2aec4197620
Theo <58122097+tzebre@users.noreply.github.com>
2023-07-18 11:19:08 -0400

    first draft of softseg test

--- dataset_conversion/bids_softseg_to_int16.py
+++ dataset_conversion/bids_softseg_to_int16.py
@@ -1,14 +1,17 @@
 import argparse
+import os
+
 import nibabel as nib
 import numpy as np
-from collections import OrderedDict
+import pathlib
 
 
-#TODO MORE TEST
+# TODO MORE TEST
 def get_parser():
     # parse command line arguments
     parser = argparse.ArgumentParser(description='Convert softseg float value to integer class value')
     parser.add_argument('--path-data', required=True, help='Path to BIDS dataset. Example: ~/data/dataset')
+    parser.add_argument('--path-out', required=True, help='Path to save img')
     parser.add_argument('--softseg', nargs='+', type=float, help='Voxel value class name (separated with space).'
                                                                  'If the label file is a soft segmentation, voxel value'
                                                                  ' will be discretize in class. Example:'
@@ -18,10 +21,9 @@ def get_parser():
     return parser
 
 
-def discretise_soft_seg(label_file, interval):
+def discretise_soft_seg(label_file, interval, out):
     """
     Discretize softseg in integer class.
-
     Args:
         label_file (str): Path to the label file
         interval (list): List with class boundary
@@ -37,24 +39,27 @@ def discretise_soft_seg(label_file, interval):
             upper_bound = interval[i + 1]
             class_voxel[(data >= lower_bound) & (data < upper_bound)] = i + 1
     voxel_img = nib.Nifti1Image(class_voxel, nifti_file.affine, nifti_file.header)
-    nib.save(voxel_img, label_file)
+    nib.save(voxel_img, out)
+
 
 def main():
     parser = get_parser()
     args = parser.parse_args()
     path_in = args.path_data
+    path_out = args.path_out
     softseg = args.softseg
     if softseg:
         for i in range(1, len(softseg)):
             if softseg[i] <= softseg[i - 1]:
                 raise ValueError(f"Softseg values {softseg} are not increasing.")
-    json_dict = OrderedDict()
-    json_dict['labels'] = {"background": 0}
-    for i, contrast_class in enumerate(softseg):
-        json_dict['labels'][f"??_{i + 1}"] = i + 1
-    print(path_in, softseg, json_dict)
-
-
+    print(path_in, softseg)
+    for sub in os.listdir(path_in):
+        if sub.startswith('sub-'):
+            for image in os.listdir(os.path.join(path_in, sub, "anat")):
+                if not image.endswith(".json"):
+                    pathlib.Path(path_out, sub, "anat").mkdir(parents=True, exist_ok=True)
+                    discretise_soft_seg(os.path.join(path_in, sub, "anat", image), softseg,
+                                        os.path.join(path_out, sub, "anat", image))
 
 
 if __name__ == '__main__':
