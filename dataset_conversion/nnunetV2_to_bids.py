cdcd72374b68bd6cae9e14b51944818c9c11f12e
Theo Mathieu <58122097+tzebre@users.noreply.github.com>
2023-08-15 12:07:17 -0400

    Update dataset_conversion/nnunetV2_to_bids.py
    Co-authored-by: Jan Valosek <39456460+valosekj@users.noreply.github.com>

--- dataset_conversion/nnunetV2_to_bids.py
+++ dataset_conversion/nnunetV2_to_bids.py
@@ -17,7 +17,7 @@ import nibabel as nib
 def get_parser():
     # parse command line arguments
     parser = argparse.ArgumentParser(description='Convert BIDS-structured dataset to nnUNetV2 database format.')
-    parser.add_argument('--path-bids', required=True, help='Path to nnUNet dataset. Example: ~/data/dataset')
+    parser.add_argument('--path-in', required=True, help='Path to nnUNet dataset. Example: ~/data/dataset')
     parser.add_argument('--path-out', required=True, help='Path to output directory. Example: ~/data/dataset-bids')
     parser.add_argument('--suffix', required=True, help='Suffix of the label file Example: sub-003_T2w_SUFFIX.nii.gz')
     parser.add_argument('--copy', '-cp', type=bool, default=False,
