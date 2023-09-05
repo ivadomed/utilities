f4506d936bf2bf809e8dc10d3cf646ef2906a4b0
valosekj <jan.valosek@upol.cz>
2023-09-05 18:13:00 -0400

    Use variables to make the code more universal

--- scripts/run_nnunet.sh
+++ scripts/run_nnunet.sh
@@ -7,15 +7,22 @@
 # Authors: Naga Karthik, Jan Valosek
 #
 
-echo "-------------------------------------------------------"
-echo "Running preprocessing and verifying dataset integrity"
-echo "-------------------------------------------------------"
-nnUNetv2_plan_and_preprocess -d <DATASET_ID> --verify_dataset_integrity -c <CONFIG>     # <CONFIG> is optional, might be 2d, 3d_fullres, 3d_lowres, or 3d_cascade_fullres
+# !!! MODIFY THE FOLLOWING VARIABLES ACCORDING TO YOUR NEEDS !!!
+config=CONFIG                     # e.g. 3d_fullres or 2d
+dataset_id=DATASET_ID             # e.g. 301
+dataset_name=DATASET_NAME         # e.g Dataset301_XXX
+nnunet_trainer="nnUNetTrainer"
 
 # Select number of folds here
 # folds=(0 1 2 3 4)
 # folds=(0 1 2)
 folds=(0)
+
+echo "-------------------------------------------------------"
+echo "Running preprocessing and verifying dataset integrity"
+echo "-------------------------------------------------------"
+
+nnUNetv2_plan_and_preprocess -d ${dataset_id} --verify_dataset_integrity
 
 for fold in ${folds[@]}; do
     echo "-------------------------------------------"
@@ -23,7 +30,7 @@ for fold in ${folds[@]}; do
     echo "-------------------------------------------"
 
     # training
-    CUDA_VISIBLE_DEVICES=1 nnUNetv2_train <DATASET_ID> <CONFIG> $fold
+    CUDA_VISIBLE_DEVICES=1 nnUNetv2_train ${dataset_id} ${config} ${fold} -tr ${nnunet_trainer}
 
     echo ""
     echo "-------------------------------------------"
@@ -31,7 +38,7 @@ for fold in ${folds[@]}; do
     echo "-------------------------------------------"
 
     # inference
-    CUDA_VISIBLE_DEVICES=1 nnUNetv2_predict -i ${nnUNet_raw}/<DATASET_ID>/imagesTs -o <OUT_DIR>/nnUNetPlans__<CONFIG>/fold_$fold/test -d <DATASET_ID> -f $fold -c <CONFIG>
+    CUDA_VISIBLE_DEVICES=1 nnUNetv2_predict -i ${nnUNet_raw}/${dataset_name}/imagesTs -tr ${nnunet_trainer} -o ${nnUNet_results}/${nnunet_trainer}__nnUNetPlans__${config}/fold_${fold}/test -d ${dataset_id} -f ${fold} -c ${config}
 
     echo ""
     echo "-------------------------------------------"
